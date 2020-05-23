5e556389973fcc52237af14386c087f080ce8666
larrygf02 <raulgf_02@hotmail.com>
2020-05-23 18:06:47 -0500

    feat: Add message of welcome

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import socketio from 'socket.io';
 import http from 'http';
 import router from './router';
+import { addUser, removeUser, getUser, getUsersInRoom } from './users';
 
 const PORT = process.env.PORT || 5000
 const app = express();
@@ -11,9 +12,13 @@ const io = socketio(server)
 io.on('connection', (socket) => {
     console.log('We have a new connection...')
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
-        const error = true;
+        const { error, user } = addUser({ id: socket.id, name, room });
+        if (error) return callback(error);
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`})
+        socket.join(user.room);
+        
     })
+
     socket.on('disconnect', () => {
         console.log('User has disconnected!!')
     })
