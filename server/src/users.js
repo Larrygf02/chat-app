0879debbcc8c27db8381ab715f58c8e892383690
larrygf02 <raulgf_02@hotmail.com>
2020-05-23 17:58:40 -0500

    Function users

--- server/src/users.js
+++ server/src/users.js
@@ -1,6 +1,6 @@
 const users = [];
 
-const addUser = ({ id, name, room }) => {
+export const addUser = ({ id, name, room }) => {
     name = name.trim().toLowerCase();
     room = room.trim().toLowerCase();
 
@@ -13,14 +13,17 @@ const addUser = ({ id, name, room }) => {
     users.push(user)
 }
 
-const removeUser = () => {
-    
+export const removeUser = (id) => {
+    const index = users.findIndex(user => user.id === id)
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
 }
 
-const getUser = () => {
-    
+export const getUser = (id) => {
+    return users.find(user => user.id === id);
 }
 
-const getUsersInRoom = () => {
-    
+export const getUsersInRoom = (room) => {
+    user.filter(user => user.room === room)
 }
