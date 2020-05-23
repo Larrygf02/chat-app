a75bab317eb7b7a4fcfd5519a131c5ed76ede283
larrygf02 <raulgf_02@hotmail.com>
2020-05-23 16:36:07 -0500

    Join component start

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
     return (
-        <h1>Join</h1>
+        <div className="joinOuterContainer">
+            <h1 className="heading">Join</h1>
+            <div>
+                <input 
+                placeholder="Name" 
+                className="joinInput" 
+                type="text" 
+                onChange={setName}/>
+            </div>
+            <div>
+                <input 
+                placeholder="Room"
+                className="joinInput"
+                type="text"
+                onChange={setRoom}/>
+            </div>
+            <Link>
+                <button className="button" type="submit">Sign In</button>
+            </Link>
+        </div>
     )
 }
 
