74e3054d64048fc479b6bb595caa4fb5a6a50604
larrygf02 <raulgf_02@hotmail.com>
2020-05-23 16:01:11 -0500

    Basic template components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Join from './components/Join';
+import Chat from './components/Chat';
 
 function App() {
   return (
