d0bb0e6fa0dc6aecda6cd91d198953ee014cd60d
larrygf02 <raulgf_02@hotmail.com>
2020-05-23 16:24:11 -0500

    feat: Implement socket io server

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import socketio from 'socket.io';
 import http from 'http';
+import router from './router';
 
 const PORT = process.env.PORT || 5000
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server)
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection...')
+    socket.on('disconnect', () => {
+        console.log('User has disconnected!!')
+    })
+})
+
+app.use(router)
 
 server.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
